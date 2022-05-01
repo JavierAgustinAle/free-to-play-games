40ede4ce1c017e33fd7b39b8f9a8955c5411fc95
Javier Ale <javierale25@live.com>
2022-05-01 17:37:48 +0200

    View to show when there is an error done

--- src/Views/GameList.tsx
+++ src/Views/GameList.tsx
@@ -1,20 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 import GameCard from './GameCard';
+import Error from './Error';
 import { GameInterface } from '../Interfaces/Game';
 
 const GameList = () => {
   // TODO: Manage Filters
 
   const [games, setGames] = useState<GameInterface[]>([]);
+  const [thereIsError, setthereIsError] = useState<Boolean>(false);
+  const [errorMsg, setError] = useState();
+
+
 
   const urlGames = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+
   const getData = () => {
     axios.request(options).then(function (response) {
       setGames(response.data);
     }).catch(function (error) {
-      // Show Error
+      setthereIsError(true);
+      setError(error);
       console.error(error);
     });
   };
@@ -33,13 +41,20 @@ const GameList = () => {
   }, [])
 
   return (
-    <div className='row pl-2 pr-2 pb-3'>
+    <>
+      {
+        thereIsError && errorMsg !== undefined ?
+          <Error props={errorMsg} />
+          :
+          <div className='row pl-2 pr-2 pb-3'>
       {
         games.map((g, index) => (
           <GameCard key={index} game={g} />
         ))
       }
     </div>
+      }
+    </>
   )
 };
 
